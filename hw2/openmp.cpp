2cd6006473e51cb49343633c9794f1c9274c98ba
Chuhan <co232@cornell.edu>
2023-03-03 14:14:34 -0800

    added initial impl for parallel

--- hw2/openmp.cpp
+++ hw2/openmp.cpp
@@ -1,14 +1,179 @@
 #include "common.h"
-#include <omp.h>
+#include <cmath>
+#include <vector>
+#include <iostream>
 
-// Put any static global variables here that you will use throughout the simulation.
+using namespace std;
+
+// define bins as an array of vector<particle_t*>
+typedef vector<particle_t*> bin_t;
+bin_t* bins;
+int numRows;
+int totalBins;
+
+// Apply the force from neighbor to particle
+void apply_force(particle_t& particle, particle_t& neighbor) {
+    // Calculate Distance
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+
+    // Check if the two particles should interact
+    if (r2 > cutoff * cutoff)
+        return;
+
+    r2 = fmax(r2, min_r * min_r);
+    double r = sqrt(r2);
+
+    // Very simple short-range repulsive force
+    double coef = (1 - cutoff / r) / r2 / mass;
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+}
+
+// Integrate the ODE
+void move(particle_t& p, double size) {
+    // Slightly simplified Velocity Verlet integration
+    // Conserves energy better than explicit Euler method
+    p.vx += p.ax * dt;
+    p.vy += p.ay * dt;
+    p.x += p.vx * dt;
+    p.y += p.vy * dt;
+
+    // Bounce from walls
+    while (p.x < 0 || p.x > size) {
+        p.x = p.x < 0 ? -p.x : 2 * size - p.x;
+        p.vx = -p.vx;
+    }
+
+    while (p.y < 0 || p.y > size) {
+        p.y = p.y < 0 ? -p.y : 2 * size - p.y;
+        p.vy = -p.vy;
+    }
+}
+
 
 void init_simulation(particle_t* parts, int num_parts, double size) {
-	// You can use this space to initialize data objects that you may need
-	// This function will be called once before the algorithm begins
-	// Do not do any particle simulation here
+	// You can use this space to initialize static, global data objects
+    // that you may need. This function will be called once before the
+    // algorithm begins. Do not do any particle simulation here
+    numRows = (size / cutoff) + 1;
+    totalBins = numRows * numRows;
+    bins = new bin_t[totalBins];
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
-    // Write this function
+    #pragma omp master
+    {
+            // clear each bins
+        for (int i = 0; i < totalBins; ++i) {
+            bins[i].clear();
+        }
+
+        // put particles into bins
+        for (int i = 0; i < num_parts; ++i) {
+            int col = parts[i].x / cutoff;
+            int row = parts[i].y / cutoff;
+            int bin = col + row * numRows;
+            bins[bin].push_back(&parts[i]);
+        }
+
+        
+        // for each particle, only apply force onto it for particles in the 9 neighboring bins
+        for (int i = 0; i < num_parts; ++i)
+        {
+            parts[i].ax = parts[i].ay = 0; 
+
+            int col = parts[i].x / cutoff;  
+            int row = parts[i].y / cutoff; 
+            int binNum = col + row * numRows;
+
+            bool hasLeft = col - 1 >= 0;
+            bool hasRight = col + 1 < numRows;
+            bool hasTop = row - 1 >= 0;
+            bool hasBottom = row + 1 < numRows;
+
+            // current bin
+            for (int j = 0; j < bins[binNum].size(); ++j)
+            {
+                apply_force(parts[i], *bins[binNum][j]);
+            
+            }
+
+            // bins in the rows above
+            if (hasLeft)
+            {
+                for (int j = 0; j < bins[binNum - 1].size(); ++j)
+                {
+                    apply_force(parts[i], *bins[binNum - 1][j]);
+                }
+            }
+
+            // bin to the right 
+            if (hasRight)
+            {
+                for (int j = 0; j < bins[binNum + 1].size(); ++j)
+                {
+                    apply_force(parts[i], *bins[binNum + 1][j]);
+                }
+            }
+
+            // bin above
+            if (hasTop)
+            {
+                for (int j = 0; j < bins[binNum - numRows].size(); ++j)
+                {
+                    apply_force(parts[i], *bins[binNum - numRows][j]);
+                }
+                
+                if (hasLeft)
+                {
+                    for (int j = 0; j < bins[binNum - numRows - 1].size(); ++j)
+                    {
+                        apply_force(parts[i], *bins[binNum - numRows - 1][j]);
+                    }
+                }
+
+                if (hasRight)
+                {
+                    for (int j = 0; j < bins[binNum - numRows + 1].size(); ++j)
+                    {
+                        apply_force(parts[i], *bins[binNum - numRows + 1][j]);
+                    }
+                }
+            }
+
+
+            if (hasBottom)
+            {
+                // bin directly below
+                for (int j = 0; j < bins[binNum + numRows].size(); ++j)
+                {
+                    apply_force(parts[i], *bins[binNum + numRows][j]);
+                }
+
+                if (hasLeft)
+                {
+                    for (int j = 0; j < bins[binNum + numRows - 1].size(); ++j)
+                    {
+                        apply_force(parts[i], *bins[binNum + numRows - 1][j]);
+                    }
+                }
+                
+                // bin directly below to the left
+                if (hasRight)
+                {
+                    for (int j = 0; j < bins[binNum + numRows + 1].size(); ++j)
+                    {
+                        apply_force(parts[i], *bins[binNum + numRows + 1][j]);
+                    }
+                }
+            }
+        }
+
+        // Move Particles
+        for (int i = 0; i < num_parts; ++i) {
+            move(parts[i], size);
+        }
+    }
 }
