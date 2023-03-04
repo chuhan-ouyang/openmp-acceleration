797a228750bbf6c90374e017128d247e018c65a7
Chuhan <co232@cornell.edu>
2023-03-04 12:45:50 -0800

    pairs parallilization work, just slow

--- hw2/openmp.cpp
+++ hw2/openmp.cpp
@@ -12,7 +12,30 @@ bin_t* bins;
 int numRows;
 int totalBins;
 omp_lock_t* lckArray;
-vector<vector<int>> dirs;
+omp_lock_t write_lock;
+
+void apply_force_pairs(particle_t& particle, particle_t& neighbor) {
+    // Calculate Distance
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+
+    // Check if the two particles should interact
+    if (r2 > cutoff * cutoff)
+        return;
+
+    r2 = fmax(r2, min_r * min_r);
+    double r = sqrt(r2);
+
+    // Very simple short-range repulsive force
+    double coef = (1 - cutoff / r) / r2 / mass;
+    omp_set_lock(&(write_lock));
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+    neighbor.ax += coef * (-dx);
+    neighbor.ay += coef * (-dy);
+    omp_unset_lock(&(write_lock));
+}
 
 // Apply the force from neighbor to particle
 void apply_force(particle_t& particle, particle_t& neighbor) {
@@ -69,8 +92,7 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
     {
         omp_init_lock(&lckArray[i]);
     }
-
-    dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    omp_init_lock(&write_lock);
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
@@ -85,29 +107,73 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     {
         int col = parts[i].x / cutoff;
         int row = parts[i].y / cutoff;
+        parts[i].ax = parts[i].ay = 0; 
         int bin = col + row * numRows;
-        parts[i].ax = parts[i].ay = 0;
         omp_set_lock(&(lckArray[bin]));
         bins[bin].push_back(&parts[i]);
         omp_unset_lock(&(lckArray[bin]));
     }
- 
-    #pragma omp for collapse(3)
-    for (int r = 0; r < numRows; ++r)
+
+    #pragma omp for
+    for (int i = 0; i < totalBins; i++)
     {
-        for (int c = 0; c < numRows; ++c)
+        for (int j = 0; j < bins[i].size(); j++)
         {
-            for (int j = 0; j < dirs.size(); j++)
+            for (int k = j + 1; k < bins[i].size(); k++)
             {
-                if (0 <= (r + dirs[j][0]) && (r + dirs[j][0]) < numRows && 0 <= (c + dirs[j][1]) && (c + dirs[j][1]) < numRows)
-                {   
-                    for (int i = 0; i < bins[(r * numRows + c)].size(); ++i)
-                    {
-                        for (int k = 0; k < bins[(r + dirs[j][0]) * numRows + (c + dirs[j][1])].size(); ++k)
-                        {
-                            apply_force(*bins[r * numRows + c][i], *bins[(r + dirs[j][0]) * numRows + (c + dirs[j][1])][k]);
-                        }
-                    }
+                apply_force_pairs(*bins[i][j], *bins[i][k]);
+            }
+        }
+    }
+
+    #pragma omp for
+    // for each particle, only apply force onto it for particles in the 9 neighboring bins
+    for (int i = 0; i < num_parts; ++i)
+    {
+
+        int col = parts[i].x / cutoff;  
+        int row = parts[i].y / cutoff; 
+        int binNum = col + row * numRows;
+
+        bool hasLeft = col - 1 >= 0;
+        bool hasRight = col + 1 < numRows;
+        bool hasTop = row - 1 >= 0;
+        bool hasBottom = row + 1 < numRows;
+
+        // current bin
+        if (hasRight)
+        {
+            for (int j = 0; j < bins[binNum + 1].size(); ++j)
+            {
+                apply_force_pairs(parts[i], *bins[binNum + 1][j]);
+            }
+        }
+
+       if (hasBottom)
+        {
+            //cout << "7" << endl;
+            // bin directly below
+            for (int j = 0; j < bins[binNum + numRows].size(); ++j)
+            {
+                apply_force_pairs(parts[i], *bins[binNum + numRows][j]);
+            }
+
+            //cout << "8" << endl;
+            if (hasLeft)
+            {
+                for (int j = 0; j < bins[binNum + numRows - 1].size(); ++j)
+                {
+                    apply_force_pairs(parts[i], *bins[binNum + numRows - 1][j]);
+                }
+            }
+            
+            // bin directly below to the left
+            //cout << "9" << endl;
+            if (hasRight)
+            {
+                for (int j = 0; j < bins[binNum + numRows + 1].size(); ++j)
+                {
+                    apply_force_pairs(parts[i], *bins[binNum + numRows + 1][j]);
                 }
             }
         }
