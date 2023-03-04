5ba919495e4ac3ca31aa3b7423fa715e1823dbda
Chuhan <co232@cornell.edu>
2023-03-04 15:51:45 -0800

    final versions for openmp and serial

--- hw2/openmp.cpp
+++ hw2/openmp.cpp
@@ -7,12 +7,39 @@
 using namespace std;
 
 // define bins as an array of vector<particle_t*>
-typedef vector<particle_t*> bin_t;
+typedef vector<pair<particle_t*, int>> bin_t;
 bin_t* bins;
 int numRows;
 int totalBins;
 omp_lock_t* lckArray;
+omp_lock_t* particlesLckArray;
 vector<vector<int>> dirs;
+
+void apply_force_pairs(particle_t& particle, particle_t& neighbor, int particleIndex, int neighborIndex) {
+    // Calculate Distance
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+
+    // Check if the two particles should interact
+    if (r2 > cutoff * cutoff)
+        return;
+
+    r2 = fmax(r2, min_r * min_r);
+    double r = sqrt(r2);
+
+    // Very simple short-range repulsive force
+    double coef = (1 - cutoff / r) / r2 / mass;
+    omp_set_lock(&(particlesLckArray[particleIndex]));
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+    omp_unset_lock(&(particlesLckArray[particleIndex]));
+
+    omp_set_lock(&(particlesLckArray[neighborIndex]));
+    neighbor.ax += coef * (-dx);
+    neighbor.ay += coef * (-dy);
+    omp_unset_lock(&(particlesLckArray[neighborIndex]));
+}
 
 // Apply the force from neighbor to particle
 void apply_force(particle_t& particle, particle_t& neighbor) {
@@ -64,13 +91,19 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
     totalBins = numRows * numRows;
     bins = new bin_t[totalBins];
     lckArray = new omp_lock_t[totalBins];
+    particlesLckArray = new omp_lock_t[num_parts];
 
-    for (int i = 0; i < totalBins; i++)
+    for (int i = 0; i < totalBins; ++i)
     {
         omp_init_lock(&lckArray[i]);
     }
 
-    dirs = {{-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
+    for (int i = 0; i < num_parts; ++i)
+    {
+        omp_init_lock(&particlesLckArray[i]);
+    }
+
+    dirs = {{0,0}, {0, 1}, {1, -1}, {1, 0}, {1, 1}};
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
@@ -88,7 +121,7 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
         int bin = col + row * numRows;
         parts[i].ax = parts[i].ay = 0;
         omp_set_lock(&(lckArray[bin]));
-        bins[bin].push_back(&parts[i]);
+        bins[bin].push_back({&parts[i], i});
         omp_unset_lock(&(lckArray[bin]));
     }
  
@@ -97,15 +130,34 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     {
         for (int c = 0; c < numRows; ++c)
         {
-            for (int j = 0; j < dirs.size(); j++)
+            for (int j = 0; j < dirs.size(); j++) // change dirs to only account for later ones
             {
-                if (0 <= (r + dirs[j][0]) && (r + dirs[j][0]) < numRows && 0 <= (c + dirs[j][1]) && (c + dirs[j][1]) < numRows)
-                {   
-                    for (int i = 0; i < bins[(r * numRows + c)].size(); ++i)
+                // current block
+                int binNum = r * numRows + c;
+                int newRow = r + dirs[j][0];
+                int newCol = c + dirs[j][1];
+
+                if (r == newRow && c == newCol)
+                {
+                    for (int i = 0; i < bins[binNum].size(); ++i)
                     {
-                        for (int k = 0; k < bins[(r + dirs[j][0]) * numRows + (c + dirs[j][1])].size(); ++k)
+                        for (int k = 0; k < bins[binNum].size(); ++k)
                         {
-                            apply_force(*bins[r * numRows + c][i], *bins[(r + dirs[j][0]) * numRows + (c + dirs[j][1])][k]);
+                            if (k > i)
+                            {
+                                apply_force_pairs(*bins[binNum][i].first, *bins[binNum][k].first, bins[binNum][i].second, bins[binNum][k].second);
+                            }
+                        }
+                    }
+                }
+                // all the other four remaining blocks
+                else if (0 <= newRow && newRow < numRows && 0 <= newCol && newCol < numRows)
+                {   
+                    for (int i = 0; i < bins[binNum].size(); ++i)
+                    {
+                        for (int k = 0; k < bins[newRow * numRows + newCol].size(); ++k)
+                        {
+                            apply_force_pairs(*bins[binNum][i].first, *bins[newRow * numRows + newCol][k].first, bins[binNum][i].second, bins[newRow * numRows + newCol][k].second);
                         }
                     }
                 }
